b434e5fc4804e1caae98449f882df4dee2675eeb
Narouv <narouv222@gmail.com>
2025-03-14 16:25:33 +0000

    finished C part

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -2,7 +2,7 @@
 /*
 	gold or red
 */
-#define CODE "#include <stdio.h>%c/*%c%cgold or red%c*/%c#define CODE %c%s%c%c#define OUTPUT_FILE %cGrace_kid.c%c%c#define GRACE() int main() { FILE *fp = fopen(OUTPUT_FILE, %cw%c); fprintf(fp, CODE, 10, 10, 9, 10, 10, 34, CODE, 34, 10, 34, 34, 10, 34, 34, 10, 10); fclose(fp); return 0; }%cGRACE()%c"
-#define OUTPUT_FILE "Grace_kid.c"
-#define GRACE() int main() { FILE *fp = fopen(OUTPUT_FILE, "w"); fprintf(fp, CODE, 10, 10, 9, 10, 10, 34, CODE, 34, 10, 34, 34, 10, 34, 34, 10, 10); fclose(fp); return 0; }
+#define CODE "#include <stdio.h>%c/*%c%cgold or red%c*/%c#define CODE %c%s%c%c#define OUTFILE %cGrace_kid.c%c%c#define GRACE() int main() { FILE *fp = fopen(OUTFILE, %cw%c); fprintf(fp, CODE, 10, 10, 9, 10, 10, 34, CODE, 34, 10, 34, 34, 10, 34, 34, 10, 10); fclose(fp); return 0; }%cGRACE()%c"
+#define OUTFILE "Grace_kid.c"
+#define GRACE() int main() { FILE *fp = fopen(OUTFILE, "w"); fprintf(fp, CODE, 10, 10, 9, 10, 10, 34, CODE, 34, 10, 34, 34, 10, 34, 34, 10, 10); fclose(fp); return 0; }
 GRACE()
